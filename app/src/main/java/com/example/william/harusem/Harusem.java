44f9e39ec6a05cbe1a8da17d5ed23d859d5573f0
unknown <rewan36.ra@gmail.com>
2018-06-29 18:03:43 +0300

    searchBar Done

--- app/src/main/java/com/example/william/harusem/Harusem.java
+++ app/src/main/java/com/example/william/harusem/Harusem.java
@@ -29,6 +29,7 @@ public class Harusem extends Application{
         instance = this;
         initializeFramework();
 
+
     }
 
 
--- app/src/main/java/com/example/william/harusem/fragments/SearchFragment.java
+++ app/src/main/java/com/example/william/harusem/fragments/SearchFragment.java
@@ -3,14 +3,33 @@ package com.example.william.harusem.fragments;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 
+
 import com.example.william.harusem.R;
-import com.example.william.harusem.ui.activities.FriendsActivity;
+import com.example.william.harusem.holder.QBUsersHolder;
 import com.example.william.harusem.ui.activities.UsersActivity;
+import com.example.william.harusem.ui.adapters.UsersAdapter;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
+import com.quickblox.chat.QBChatService;
+import com.quickblox.core.QBEntityCallback;
+import com.quickblox.core.exception.QBResponseException;
+import com.quickblox.users.QBUsers;
+import com.quickblox.users.model.QBUser;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,9 +39,17 @@ import butterknife.Unbinder;
 
 public class SearchFragment extends Fragment {
 
-    @BindView(R.id.all_users_btn)
-    Button allUsersBtn;
+    private static final String TAG = UsersActivity.class.getSimpleName();
+
     Unbinder unbinder;
+    @BindView(R.id.search_view) MaterialSearchView searchView;
+    @BindView(R.id.toolbar_search) Toolbar toolbar;
+    @BindView(R.id.search_recyclerview)
+    RecyclerView recyclerView;
+
+
+    private static ArrayList<QBUser> qbUserWithoutCurrent = new ArrayList<>();
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,8 +57,113 @@ public class SearchFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_search, container, false);
         unbinder = ButterKnife.bind(this, view);
+
+        if (getActivity() != null) {
+            ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        }
+        setHasOptionsMenu(true);
+
+        retrieveAllUser();
+        configRecyclerView();
+
+
+        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+                retrieveAllUser();
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+                retrieveAllUser();
+            }
+        });
+
+
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if(query != null && !query.isEmpty()){
+                    QBUsers.getUsersByFullName(query,null).performAsync(new QBEntityCallback<ArrayList<QBUser>>() {
+                        @Override
+                        public void onSuccess(ArrayList<QBUser> qbUsers, Bundle bundle) {
+
+
+                            QBUsersHolder.getInstance().putUsers(qbUsers);
+
+                            ArrayList<QBUser> qbUserWithoutCurrent = new ArrayList<>();
+                            for (QBUser user : qbUsers) {
+                                if (!user.getLogin().equals(QBChatService.getInstance().getUser().getLogin())) {
+                                    qbUserWithoutCurrent.add(user);
+                                }
+                            }
+
+                            UsersAdapter adapter = new UsersAdapter(qbUserWithoutCurrent, getContext());
+                            recyclerView.setAdapter(adapter);
+                            adapter.notifyDataSetChanged();
+
+                        }
+
+                        @Override
+                        public void onError(QBResponseException e) {
+                            Log.e(TAG, "1 onError: ", e);
+                        }
+                    });
+
+                }
+                else {
+                    // if search text is null
+                    // return default
+                    UsersAdapter adapter = new UsersAdapter(qbUserWithoutCurrent, getContext());
+                    recyclerView.setAdapter(adapter);
+                }
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(final String newText) {
+
+                if(newText != null && !newText.isEmpty()){
+                    QBUsers.getUsersByFullName(newText,null).performAsync(new QBEntityCallback<ArrayList<QBUser>>() {
+                        @Override
+                        public void onSuccess(ArrayList<QBUser> qbUsers, Bundle bundle) {
+
+
+                            QBUsersHolder.getInstance().putUsers(qbUsers);
+
+                            ArrayList<QBUser> qbUserWithoutCurrent = new ArrayList<>();
+                            for (QBUser user : qbUsers) {
+                                if (!user.getLogin().equals(QBChatService.getInstance().getUser().getLogin())) {
+                                    qbUserWithoutCurrent.add(user);
+                                }
+                            }
+
+                            UsersAdapter adapter = new UsersAdapter(qbUserWithoutCurrent, getContext());
+                            recyclerView.setAdapter(adapter);
+                            adapter.notifyDataSetChanged();
+
+                        }
+
+                        @Override
+                        public void onError(QBResponseException e) {
+                            Log.e(TAG, "1 onError: ", e);
+                        }
+                    });
+
+                }
+                else {
+                    // if search text is null
+                    // return default
+                    UsersAdapter adapter = new UsersAdapter(qbUserWithoutCurrent, getContext());
+                    recyclerView.setAdapter(adapter);
+                }
+                return true;
+            }
+        });
+
         return view;
     }
+
 
     @Override
     public void onDestroyView() {
@@ -39,10 +171,60 @@ public class SearchFragment extends Fragment {
         unbinder.unbind();
     }
 
-    @OnClick(R.id.all_users_btn)
-    public void setAllUsersBtn(View view) {
-        Intent intent = new Intent(getActivity(), UsersActivity.class);
-        startActivity(intent);
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.chat_search_menu_item, menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        searchView.setMenuItem(item);
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                searchView.setMenuItem(item);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    public void retrieveAllUser() {
+
+        QBUsers.getUsers(null).performAsync(new QBEntityCallback<ArrayList<QBUser>>() {
+            @Override
+            public void onSuccess(ArrayList<QBUser> qbUsers, Bundle bundle) {
+                QBUsersHolder.getInstance().putUsers(qbUsers);
+
+                ArrayList<QBUser> qbUserWithoutCurrent = new ArrayList<>();
+                for (QBUser user : qbUsers) {
+                    if (!user.getLogin().equals(QBChatService.getInstance().getUser().getLogin())) {
+                        qbUserWithoutCurrent.add(user);
+                    }
+                }
+
+                UsersAdapter adapter = new UsersAdapter(qbUserWithoutCurrent, getContext());
+                recyclerView.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onError(QBResponseException e) {
+                Log.e(TAG, "1 onError: ", e);
+            }
+        });
+
+    }
+
+    private void configRecyclerView() {
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(mLayoutManager);
     }
 
 
